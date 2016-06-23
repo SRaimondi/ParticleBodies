28533fe5fe6851803e51d54d472f1227288f1af0
Simone Raimondi <simo@cleverit.ch>
2016-06-23 19:51:08 +0200

    Addded quaternions
    Added quaternion class, working on generic body interface

---
+++ ParticleBodies/include/body.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   body.hpp
+ * Author: Simon
+ *
+ * Created on June 23, 2016, 6:36 PM
+ */
+
+#ifndef BODY_HPP
+#define BODY_HPP
+
+#include "particle.hpp"
+
+namespace pb {
+    
+    // This class represents the base class that all body must implement in order
+    // to be added to the simulation
+    
+}// pb namespace
+
+#endif /* BODY_HPP */
+
